91d6252f569aad46b3f6c2b4846c963b1183632e
AlexChen <1274812218@qq.com>
2023-02-25 17:08:38 +0800

    feat:增加 Spring Security 示例模块

---
+++ springboot/springboot-security/src/main/java/tech/alexchen/springboot/security/config/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package tech.alexchen.springboot.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author alexchen
+ * @date 2023/2/25
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+}
