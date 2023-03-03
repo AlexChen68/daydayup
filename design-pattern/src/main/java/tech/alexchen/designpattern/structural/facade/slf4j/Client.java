726cedd10d444b3158bc29f0540c4da260436b88
AlexChen <1274812218@qq.com>
2023-03-03 12:03:48 +0800

    feat:增加外观设计模式

---
+++ design-pattern/src/main/java/tech/alexchen/designpattern/structural/facade/slf4j/Client.java
@@ -0,0 +1,13 @@
+package tech.alexchen.designpattern.structural.facade.slf4j;
+
+/**
+ * @author alexchen
+ * @date 2023/3/3
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Client.class);
+        logger.info("log test");
+    }
+}
