692037aae972726997c4aee979bd0df0417ca52e
AlexChen <1274812218@qq.com>
2023-03-04 16:00:42 +0800

    feat:增加多种设计模式代码实例

---
+++ design-pattern/src/main/java/tech/alexchen/designpattern/structural/bridge/Connection.java
@@ -0,0 +1,10 @@
+package tech.alexchen.designpattern.structural.bridge;
+
+/**
+ * @author alexchen
+ * @date 2023/3/3
+ */
+public interface Connection {
+
+    void prepareStatement();
+}
---
+++ design-pattern/src/main/java/tech/alexchen/designpattern/structural/bridge/DriverManager.java
@@ -0,0 +1,33 @@
+package tech.alexchen.designpattern.structural.bridge;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+
+import java.sql.SQLException;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @author alexchen
+ * @date 2023/3/3
+ */
+public abstract class DriverManager {
+
+    private static CopyOnWriteArrayList<Driver> registeredDrivers = new CopyOnWriteArrayList<>();
+    public static void registerDriver(Driver driver) {
+        registeredDrivers.addIfAbsent(driver);
+    }
+
+    @SneakyThrows
+    public static Driver getDriver(String url) {
+        for (Driver driver : registeredDrivers) {
+            if (driver.acceptsURL(url)) {
+                return driver;
+            }
+        }
+        throw new SQLException("No suitable driver");
+    }
+    public static Connection getConnection(String url) {
+       return getDriver(url).connection(url);
+    }
+}
