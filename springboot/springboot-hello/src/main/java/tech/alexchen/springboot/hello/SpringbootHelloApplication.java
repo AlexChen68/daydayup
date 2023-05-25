a6629a107252b53c0ffacd10743fda97e888c91e
alexchen <1274812218@qq.com>
2023-05-25 23:45:42 +0800

    feat: 增加 docker 打包 SpringBoot Demo

--- springboot/springboot-hello/src/main/java/tech/alexchen/springboot/hello/SpringbootHelloApplication.java
+++ springboot/springboot-hello/src/main/java/tech/alexchen/springboot/hello/SpringbootHelloApplication.java
@@ -3,6 +3,9 @@ package tech.alexchen.springboot.hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author alexchen
+ */
 @SpringBootApplication
 public class SpringbootHelloApplication {
 
